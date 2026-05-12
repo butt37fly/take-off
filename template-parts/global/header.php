864dffe4751ccb8fd7c54c4a6b298c44f550372e
Andrés Ospina <adrosscosmic@gmail.com>
2026-05-12 17:28:37 -0500

    refactor: estilizar con clases globales

--- template-parts/global/header.php
+++ template-parts/global/header.php
@@ -1,5 +1,5 @@
 <header class="header">
-    <section class="header__wrapper">
+    <section class="header__wrapper u-flex u-row u-justify-between u-align-center u-g-2 u-p-2 u-m-auto">
         <div class="header__box header--left">
 
             <?php b37_display_custom_logo() ?>
